e5651273477a1fe5fa338a975ecfdeaee435777e
Huahui Mo <33701207+ChristianMok@users.noreply.github.com>
2020-01-29 16:57:37 -0700

    Updates

---
+++ precache-manifest.a5f44e3218753ec90699b639e2aa617c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a22c709188eb311af1bfd2091c5a8965",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "f18a50c5a2d93d2c354f",
+    "url": "/monsters-rolodex/static/css/main.83d3ed63.chunk.css"
+  },
+  {
+    "revision": "c68e14fdc7e1ba4a5621",
+    "url": "/monsters-rolodex/static/js/2.9e7d0785.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.9e7d0785.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f18a50c5a2d93d2c354f",
+    "url": "/monsters-rolodex/static/js/main.deb022d6.chunk.js"
+  },
+  {
+    "revision": "9e014af7b12dc10ef867",
+    "url": "/monsters-rolodex/static/js/runtime-main.ff038b7c.js"
+  }
+]);
